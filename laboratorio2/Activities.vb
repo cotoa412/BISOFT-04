29dd2e9360fae47681f6c631f4e31595877efef8
laboratorio <laboratorio@SP-LAB9-17>
2018-10-23 20:50:50 -0600

    Creé la clase Activities

---
+++ laboratorio2/Activities.vb
@@ -0,0 +1,34 @@
+﻿Imports laboratorio2
+
+Public Class Activities
+    Private Name As String
+    Private Date_hours As DateTime
+    Private Course As Course
+
+    Public Property Name1 As String
+        Get
+            Return Name
+        End Get
+        Set(value As String)
+            Name = value
+        End Set
+    End Property
+
+    Public Property Date_hours1 As Date
+        Get
+            Return Date_hours
+        End Get
+        Set(value As Date)
+            Date_hours = value
+        End Set
+    End Property
+
+    Public Property Course1 As Course
+        Get
+            Return Course
+        End Get
+        Set(value As Course)
+            Course = value
+        End Set
+    End Property
+End Class
---
+++ laboratorio2/ClassHorario.vb
@@ -0,0 +1,6 @@
+﻿Public Class ClassHorario
+
+
+
+
+End Class
