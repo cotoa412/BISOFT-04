928ef94fa990231ff7d12c6a6d83b3d287f4e094
Jorge Vargas <jorgev259@outlook.es>
2018-10-28 16:00:47 -0600

    fix conflicts

--- laboratorio2/Calendario.Designer.vb
+++ laboratorio2/Calendario.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Calendario
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -48,11 +49,20 @@ Partial Class Calendario
         Me.PictureBox2.TabIndex = 1
         Me.PictureBox2.TabStop = False
         '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(23, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 58)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
         'Calendario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(632, 344)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Calendario"
@@ -65,4 +75,5 @@ Partial Class Calendario
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
 End Class
--- laboratorio2/Calendario.vb
+++ laboratorio2/Calendario.vb
@@ -2,4 +2,11 @@
     Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs)
 
     End Sub
+
+    Private Sub Calendario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim firstDay As DateTime
+        firstDay = New DateTime(Now.Year, Now.Month, 1)
+        Label1.Text = Date.Today.DayOfWeek.ToString()
+
+    End Sub
 End Class
