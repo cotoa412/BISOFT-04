cb9c3a3ee7b1e93e7711d98b27bb84063ac919f2
Jorge Vargas <jorgev259@outlook.es>
2018-11-05 10:48:18 -0600

    terminar calendario

--- laboratorio2/Calendario.Designer.vb
+++ laboratorio2/Calendario.Designer.vb
@@ -24,7 +24,41 @@ Partial Class Calendario
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.d1_1 = New System.Windows.Forms.Label()
+        Me.d1_2 = New System.Windows.Forms.Label()
+        Me.d1_3 = New System.Windows.Forms.Label()
+        Me.d1_4 = New System.Windows.Forms.Label()
+        Me.d1_5 = New System.Windows.Forms.Label()
+        Me.d1_6 = New System.Windows.Forms.Label()
+        Me.d1_7 = New System.Windows.Forms.Label()
+        Me.d2_7 = New System.Windows.Forms.Label()
+        Me.d2_6 = New System.Windows.Forms.Label()
+        Me.d2_5 = New System.Windows.Forms.Label()
+        Me.d2_4 = New System.Windows.Forms.Label()
+        Me.d2_3 = New System.Windows.Forms.Label()
+        Me.d2_2 = New System.Windows.Forms.Label()
+        Me.d2_1 = New System.Windows.Forms.Label()
+        Me.d3_7 = New System.Windows.Forms.Label()
+        Me.d3_6 = New System.Windows.Forms.Label()
+        Me.d3_5 = New System.Windows.Forms.Label()
+        Me.d3_4 = New System.Windows.Forms.Label()
+        Me.d3_3 = New System.Windows.Forms.Label()
+        Me.d3_2 = New System.Windows.Forms.Label()
+        Me.d3_1 = New System.Windows.Forms.Label()
+        Me.d4_7 = New System.Windows.Forms.Label()
+        Me.d4_6 = New System.Windows.Forms.Label()
+        Me.d4_5 = New System.Windows.Forms.Label()
+        Me.d4_4 = New System.Windows.Forms.Label()
+        Me.d4_3 = New System.Windows.Forms.Label()
+        Me.d4_2 = New System.Windows.Forms.Label()
+        Me.d4_1 = New System.Windows.Forms.Label()
+        Me.d5_7 = New System.Windows.Forms.Label()
+        Me.d5_6 = New System.Windows.Forms.Label()
+        Me.d5_5 = New System.Windows.Forms.Label()
+        Me.d5_4 = New System.Windows.Forms.Label()
+        Me.d5_3 = New System.Windows.Forms.Label()
+        Me.d5_2 = New System.Windows.Forms.Label()
+        Me.d5_1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -49,20 +83,291 @@ Partial Class Calendario
         Me.PictureBox2.TabIndex = 1
         Me.PictureBox2.TabStop = False
         '
-        'Label1
+        'd1_1
         '
-        Me.Label1.Location = New System.Drawing.Point(23, 26)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(83, 58)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.d1_1.Location = New System.Drawing.Point(22, 25)
+        Me.d1_1.Name = "d1_1"
+        Me.d1_1.Size = New System.Drawing.Size(83, 56)
+        Me.d1_1.TabIndex = 2
+        '
+        'd1_2
+        '
+        Me.d1_2.Location = New System.Drawing.Point(108, 25)
+        Me.d1_2.Name = "d1_2"
+        Me.d1_2.Size = New System.Drawing.Size(79, 56)
+        Me.d1_2.TabIndex = 3
+        '
+        'd1_3
+        '
+        Me.d1_3.Location = New System.Drawing.Point(193, 26)
+        Me.d1_3.Name = "d1_3"
+        Me.d1_3.Size = New System.Drawing.Size(79, 56)
+        Me.d1_3.TabIndex = 4
+        '
+        'd1_4
+        '
+        Me.d1_4.Location = New System.Drawing.Point(278, 25)
+        Me.d1_4.Name = "d1_4"
+        Me.d1_4.Size = New System.Drawing.Size(81, 56)
+        Me.d1_4.TabIndex = 5
+        '
+        'd1_5
+        '
+        Me.d1_5.Location = New System.Drawing.Point(363, 26)
+        Me.d1_5.Name = "d1_5"
+        Me.d1_5.Size = New System.Drawing.Size(81, 56)
+        Me.d1_5.TabIndex = 6
+        '
+        'd1_6
+        '
+        Me.d1_6.Location = New System.Drawing.Point(448, 26)
+        Me.d1_6.Name = "d1_6"
+        Me.d1_6.Size = New System.Drawing.Size(81, 56)
+        Me.d1_6.TabIndex = 7
+        '
+        'd1_7
+        '
+        Me.d1_7.Location = New System.Drawing.Point(532, 25)
+        Me.d1_7.Name = "d1_7"
+        Me.d1_7.Size = New System.Drawing.Size(81, 56)
+        Me.d1_7.TabIndex = 8
+        '
+        'd2_7
+        '
+        Me.d2_7.Location = New System.Drawing.Point(532, 84)
+        Me.d2_7.Name = "d2_7"
+        Me.d2_7.Size = New System.Drawing.Size(81, 56)
+        Me.d2_7.TabIndex = 15
+        '
+        'd2_6
+        '
+        Me.d2_6.Location = New System.Drawing.Point(448, 85)
+        Me.d2_6.Name = "d2_6"
+        Me.d2_6.Size = New System.Drawing.Size(81, 56)
+        Me.d2_6.TabIndex = 14
+        '
+        'd2_5
+        '
+        Me.d2_5.Location = New System.Drawing.Point(363, 85)
+        Me.d2_5.Name = "d2_5"
+        Me.d2_5.Size = New System.Drawing.Size(81, 56)
+        Me.d2_5.TabIndex = 13
+        '
+        'd2_4
+        '
+        Me.d2_4.Location = New System.Drawing.Point(278, 84)
+        Me.d2_4.Name = "d2_4"
+        Me.d2_4.Size = New System.Drawing.Size(81, 56)
+        Me.d2_4.TabIndex = 12
+        '
+        'd2_3
+        '
+        Me.d2_3.Location = New System.Drawing.Point(193, 85)
+        Me.d2_3.Name = "d2_3"
+        Me.d2_3.Size = New System.Drawing.Size(79, 56)
+        Me.d2_3.TabIndex = 11
+        '
+        'd2_2
+        '
+        Me.d2_2.Location = New System.Drawing.Point(108, 84)
+        Me.d2_2.Name = "d2_2"
+        Me.d2_2.Size = New System.Drawing.Size(79, 56)
+        Me.d2_2.TabIndex = 10
+        '
+        'd2_1
+        '
+        Me.d2_1.Location = New System.Drawing.Point(22, 84)
+        Me.d2_1.Name = "d2_1"
+        Me.d2_1.Size = New System.Drawing.Size(83, 56)
+        Me.d2_1.TabIndex = 9
+        '
+        'd3_7
+        '
+        Me.d3_7.Location = New System.Drawing.Point(533, 146)
+        Me.d3_7.Name = "d3_7"
+        Me.d3_7.Size = New System.Drawing.Size(81, 56)
+        Me.d3_7.TabIndex = 22
+        '
+        'd3_6
+        '
+        Me.d3_6.Location = New System.Drawing.Point(449, 147)
+        Me.d3_6.Name = "d3_6"
+        Me.d3_6.Size = New System.Drawing.Size(81, 56)
+        Me.d3_6.TabIndex = 21
+        '
+        'd3_5
+        '
+        Me.d3_5.Location = New System.Drawing.Point(364, 147)
+        Me.d3_5.Name = "d3_5"
+        Me.d3_5.Size = New System.Drawing.Size(81, 56)
+        Me.d3_5.TabIndex = 20
+        '
+        'd3_4
+        '
+        Me.d3_4.Location = New System.Drawing.Point(279, 146)
+        Me.d3_4.Name = "d3_4"
+        Me.d3_4.Size = New System.Drawing.Size(81, 56)
+        Me.d3_4.TabIndex = 19
+        '
+        'd3_3
+        '
+        Me.d3_3.Location = New System.Drawing.Point(194, 147)
+        Me.d3_3.Name = "d3_3"
+        Me.d3_3.Size = New System.Drawing.Size(79, 56)
+        Me.d3_3.TabIndex = 18
+        '
+        'd3_2
+        '
+        Me.d3_2.Location = New System.Drawing.Point(109, 146)
+        Me.d3_2.Name = "d3_2"
+        Me.d3_2.Size = New System.Drawing.Size(79, 56)
+        Me.d3_2.TabIndex = 17
+        '
+        'd3_1
+        '
+        Me.d3_1.Location = New System.Drawing.Point(23, 146)
+        Me.d3_1.Name = "d3_1"
+        Me.d3_1.Size = New System.Drawing.Size(83, 56)
+        Me.d3_1.TabIndex = 16
+        '
+        'd4_7
+        '
+        Me.d4_7.Location = New System.Drawing.Point(533, 206)
+        Me.d4_7.Name = "d4_7"
+        Me.d4_7.Size = New System.Drawing.Size(81, 56)
+        Me.d4_7.TabIndex = 29
+        '
+        'd4_6
+        '
+        Me.d4_6.Location = New System.Drawing.Point(449, 207)
+        Me.d4_6.Name = "d4_6"
+        Me.d4_6.Size = New System.Drawing.Size(81, 56)
+        Me.d4_6.TabIndex = 28
+        '
+        'd4_5
+        '
+        Me.d4_5.Location = New System.Drawing.Point(364, 207)
+        Me.d4_5.Name = "d4_5"
+        Me.d4_5.Size = New System.Drawing.Size(81, 56)
+        Me.d4_5.TabIndex = 27
+        '
+        'd4_4
+        '
+        Me.d4_4.Location = New System.Drawing.Point(279, 206)
+        Me.d4_4.Name = "d4_4"
+        Me.d4_4.Size = New System.Drawing.Size(81, 56)
+        Me.d4_4.TabIndex = 26
+        '
+        'd4_3
+        '
+        Me.d4_3.Location = New System.Drawing.Point(194, 207)
+        Me.d4_3.Name = "d4_3"
+        Me.d4_3.Size = New System.Drawing.Size(79, 56)
+        Me.d4_3.TabIndex = 25
+        '
+        'd4_2
+        '
+        Me.d4_2.Location = New System.Drawing.Point(109, 206)
+        Me.d4_2.Name = "d4_2"
+        Me.d4_2.Size = New System.Drawing.Size(79, 56)
+        Me.d4_2.TabIndex = 24
+        '
+        'd4_1
+        '
+        Me.d4_1.Location = New System.Drawing.Point(23, 206)
+        Me.d4_1.Name = "d4_1"
+        Me.d4_1.Size = New System.Drawing.Size(83, 56)
+        Me.d4_1.TabIndex = 23
+        '
+        'd5_7
+        '
+        Me.d5_7.Location = New System.Drawing.Point(533, 267)
+        Me.d5_7.Name = "d5_7"
+        Me.d5_7.Size = New System.Drawing.Size(81, 56)
+        Me.d5_7.TabIndex = 36
+        '
+        'd5_6
+        '
+        Me.d5_6.Location = New System.Drawing.Point(448, 267)
+        Me.d5_6.Name = "d5_6"
+        Me.d5_6.Size = New System.Drawing.Size(81, 56)
+        Me.d5_6.TabIndex = 35
+        '
+        'd5_5
+        '
+        Me.d5_5.Location = New System.Drawing.Point(364, 267)
+        Me.d5_5.Name = "d5_5"
+        Me.d5_5.Size = New System.Drawing.Size(81, 56)
+        Me.d5_5.TabIndex = 34
+        '
+        'd5_4
+        '
+        Me.d5_4.Location = New System.Drawing.Point(280, 267)
+        Me.d5_4.Name = "d5_4"
+        Me.d5_4.Size = New System.Drawing.Size(81, 56)
+        Me.d5_4.TabIndex = 33
+        '
+        'd5_3
+        '
+        Me.d5_3.Location = New System.Drawing.Point(195, 267)
+        Me.d5_3.Name = "d5_3"
+        Me.d5_3.Size = New System.Drawing.Size(79, 56)
+        Me.d5_3.TabIndex = 32
+        '
+        'd5_2
+        '
+        Me.d5_2.Location = New System.Drawing.Point(111, 266)
+        Me.d5_2.Name = "d5_2"
+        Me.d5_2.Size = New System.Drawing.Size(79, 56)
+        Me.d5_2.TabIndex = 31
+        '
+        'd5_1
+        '
+        Me.d5_1.Location = New System.Drawing.Point(22, 266)
+        Me.d5_1.Name = "d5_1"
+        Me.d5_1.Size = New System.Drawing.Size(83, 56)
+        Me.d5_1.TabIndex = 30
         '
         'Calendario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(632, 344)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.d5_7)
+        Me.Controls.Add(Me.d5_6)
+        Me.Controls.Add(Me.d5_5)
+        Me.Controls.Add(Me.d5_4)
+        Me.Controls.Add(Me.d5_3)
+        Me.Controls.Add(Me.d5_2)
+        Me.Controls.Add(Me.d5_1)
+        Me.Controls.Add(Me.d4_7)
+        Me.Controls.Add(Me.d4_6)
+        Me.Controls.Add(Me.d4_5)
+        Me.Controls.Add(Me.d4_4)
+        Me.Controls.Add(Me.d4_3)
+        Me.Controls.Add(Me.d4_2)
+        Me.Controls.Add(Me.d4_1)
+        Me.Controls.Add(Me.d3_7)
+        Me.Controls.Add(Me.d3_6)
+        Me.Controls.Add(Me.d3_5)
+        Me.Controls.Add(Me.d3_4)
+        Me.Controls.Add(Me.d3_3)
+        Me.Controls.Add(Me.d3_2)
+        Me.Controls.Add(Me.d3_1)
+        Me.Controls.Add(Me.d2_7)
+        Me.Controls.Add(Me.d2_6)
+        Me.Controls.Add(Me.d2_5)
+        Me.Controls.Add(Me.d2_4)
+        Me.Controls.Add(Me.d2_3)
+        Me.Controls.Add(Me.d2_2)
+        Me.Controls.Add(Me.d2_1)
+        Me.Controls.Add(Me.d1_7)
+        Me.Controls.Add(Me.d1_6)
+        Me.Controls.Add(Me.d1_5)
+        Me.Controls.Add(Me.d1_4)
+        Me.Controls.Add(Me.d1_3)
+        Me.Controls.Add(Me.d1_2)
+        Me.Controls.Add(Me.d1_1)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Calendario"
@@ -75,5 +380,39 @@ Partial Class Calendario
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents d1_1 As Label
+    Friend WithEvents d1_2 As Label
+    Friend WithEvents d1_3 As Label
+    Friend WithEvents d1_4 As Label
+    Friend WithEvents d1_5 As Label
+    Friend WithEvents d1_6 As Label
+    Friend WithEvents d1_7 As Label
+    Friend WithEvents d2_7 As Label
+    Friend WithEvents d2_6 As Label
+    Friend WithEvents d2_5 As Label
+    Friend WithEvents d2_4 As Label
+    Friend WithEvents d2_3 As Label
+    Friend WithEvents d2_2 As Label
+    Friend WithEvents d2_1 As Label
+    Friend WithEvents d3_7 As Label
+    Friend WithEvents d3_6 As Label
+    Friend WithEvents d3_5 As Label
+    Friend WithEvents d3_4 As Label
+    Friend WithEvents d3_3 As Label
+    Friend WithEvents d3_2 As Label
+    Friend WithEvents d3_1 As Label
+    Friend WithEvents d4_7 As Label
+    Friend WithEvents d4_6 As Label
+    Friend WithEvents d4_5 As Label
+    Friend WithEvents d4_4 As Label
+    Friend WithEvents d4_3 As Label
+    Friend WithEvents d4_2 As Label
+    Friend WithEvents d4_1 As Label
+    Friend WithEvents d5_7 As Label
+    Friend WithEvents d5_6 As Label
+    Friend WithEvents d5_5 As Label
+    Friend WithEvents d5_4 As Label
+    Friend WithEvents d5_3 As Label
+    Friend WithEvents d5_2 As Label
+    Friend WithEvents d5_1 As Label
 End Class
